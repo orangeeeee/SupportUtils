97ad698fc4c0c664a73c17656272152146d6f807
orangeeeee <orangeeeee.ore.ore@gmail.com>
2018-05-26 22:45:50 +0900

    add new file Support.java

--- src/main/java/utils/Support.java
+++ src/main/java/utils/Support.java
@@ -1,4 +1,29 @@
 package utils;
 
+import java.math.BigDecimal;
+import java.util.function.Function;
+
 public class Support {
+
+    public static Integer ofInt(String s) {
+        return Integer.valueOf(s);
+    }
+
+    public static BigDecimal decimal(final String s) {
+        return new BigDecimal(s);
+    }
+
+    public static BigDecimal decimal(long l) {
+
+        return BigDecimal.valueOf(l);
+    }
+
+    public static void decimal(double d) {
+
+        BigDecimal.valueOf(d);
+    }
+
+    public static Function<String, BigDecimal> decimal() {
+        return Support::decimal;
+    }
 }
